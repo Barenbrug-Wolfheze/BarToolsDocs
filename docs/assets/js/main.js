84230004e759c536ed3d26d21fc465bbc14f528e
ElcovRijswijk <evanrijswijk@barenbrug.com>
2023-03-22 16:57:22 +0100

    version update

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-	
+$(document).ready(function () {
+
     /* ===== Stickyfill ===== */
     /* Ref: https://github.com/wilddeer/stickyfill */
     // Add browser support to position: sticky
@@ -9,26 +9,59 @@ $(document).ready(function() {
 
     /* Activate scrollspy menu */
     $('body').scrollspy({target: '#doc-menu', offset: 100});
-    
+
     /* Smooth scrolling */
-	$('a.scrollto').on('click', function(e){
+    $('a.scrollto').on('click', function (e) {
         //store hash
-        var target = this.hash;    
+        var target = this.hash;
         e.preventDefault();
-		$('body').scrollTo(target, 800, {offset: 0, 'axis':'y'});
-		
-	});
-     
+        $('body').scrollTo(target, 800, {offset: 0, 'axis': 'y'});
+
+    });
+
     /* Bootstrap lightbox */
     /* Ref: http://ashleydw.github.io/lightbox/ */
 
-    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function (e) {
         e.preventDefault();
         $(this).ekkoLightbox();
-    });    
-
-
+    });
 
 
 });
+
+
+const copyButtonLabel = "Copy Code";
+
+// use a class selector if available
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+    // only add button if browser supports Clipboard API
+    if (navigator.clipboard) {
+        let button = document.createElement("button");
+
+        button.innerText = copyButtonLabel;
+        block.appendChild(button);
+
+        button.addEventListener("click", async () => {
+            await copyCode(block, button);
+        });
+    }
+});
+
+async function copyCode(block, button) {
+    let code = block.querySelector("code");
+    let text = code.innerText;
+
+    await navigator.clipboard.writeText(text);
+
+    // visual feedback that task is completed
+    button.innerText = "Code Copied";
+
+    setTimeout(() => {
+        button.innerText = copyButtonLabel;
+    }, 700);
+}
+
 
--- docs/assets/js/version_graph.js
+++ docs/assets/js/version_graph.js
@@ -1,74 +1,110 @@
 var options = {
-          series: [
-          {
+    series: [
+        {
             name: 'Elco',
 
 
             data: [
                 {
-                x: 'v1.1',
-                y: [
-                  new Date('2022-2-22').getTime(),
-                  new Date().getTime()
-                ]
-              },
-              {
-                x: 'v1.0',
-                y: [
-                  new Date('2021-6-01').getTime(),
-                  new Date('2022-2-22').getTime()
-                ]
-              },
-              {
-                x: 'v0.5-v1.0 Development',
-                y: [
-                  new Date('2021-1-04').getTime(),
-                  new Date('2021-6-01').getTime()
-                ]
-              },
-              {
-                x: 'Github Pre-build',
-                y: [
-                  new Date('2020-12-20').getTime(),
-                    new Date('2021-1-11').getTime()
-                ]
-              },
-              {
-                x: 'Pre-build (Bio-functions)',
-                y: [
-                  new Date('2020-8-01').getTime(),
-                  new Date('2020-12-26').getTime()
-                ]
-              }
-            ]
-          },
+                    x: 'BarTools v2.0',
+                    y: [
+                        new Date('2023-3-22').getTime(),
+                        new Date().getTime()
+                    ]
+                },
 
-        ],
-          chart: {
-          height: 450,
-          type: 'rangeBar'
+                {
+                    x: 'BarTools v1.6',
+                    y: [
+                        new Date('2022-10-13').getTime(),
+                        new Date('2023-3-22').getTime()
+                    ]
+                },
+                {
+                    x: 'BarTools v1.5',
+                    y: [
+                        new Date('2022-8-8').getTime(),
+                        new Date('2022-10-13').getTime()
+                    ]
+                },
+                {
+                    x: 'BarTools v1.4',
+                    y: [
+                        new Date('2022-5-5').getTime(),
+                        new Date('2022-8-8').getTime()
+                    ]
+                },
+                {
+                    x: 'BarTools v1.3',
+                    y: [
+                        new Date('2022-3-25').getTime(),
+                        new Date('2022-5-5').getTime(),
+                    ]
+                },
+                {
+                    x: 'BarTools v1.1',
+                    y: [
+                        new Date('2022-2-22').getTime(),
+                        new Date('2022-3-25').getTime(),
+                    ]
+                },
+                {
+                    x: 'BarTools v1.0',
+                    y: [
+                        new Date('2021-6-01').getTime(),
+                        new Date('2022-2-22').getTime()
+                    ]
+                },
+                {
+                    x: 'v0.5-v1.0 Development',
+                    y: [
+                        new Date('2021-1-04').getTime(),
+                        new Date('2021-6-01').getTime()
+                    ]
+                },
+                {
+                    x: 'Github Pre-build',
+                    y: [
+                        new Date('2020-12-20').getTime(),
+                        new Date('2021-1-11').getTime()
+                    ]
+                },
+                {
+                    x: 'Pre-build (Bio-functions)',
+                    y: [
+                        new Date('2020-8-01').getTime(),
+                        new Date('2020-12-26').getTime()
+                    ]
+                }
+            ]
         },
-        plotOptions: {
-          bar: {
+
+    ],
+    chart: {
+        height: 450,
+        type: 'rangeBar'
+    },
+    plotOptions: {
+        bar: {
             horizontal: true,
             barHeight: '80%'
-          }
-        },
-        xaxis: {
-          type: 'datetime'
-        },
-        stroke: {
-          width: 1
-        },
-        fill: {
-          type: 'solid',
-          opacity: 0.6
-        },
-        legend: {
-          position: 'top',
-          horizontalAlign: 'left'
         }
-        };
+    },
+    xaxis: {
+        type: 'datetime'
+    },
+    stroke: {
+        width: 1
+    },
+    fill: {
+        type: 'solid',
+        opacity: 0.6
+    },
+    legend: {
+        position: 'top',
+        horizontalAlign: 'left'
+    }
+};
 
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        chart.render();
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
